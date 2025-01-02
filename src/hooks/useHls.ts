e63bf38f3c3b2205cc55dabddacc4f9fbe8abb03
dott <aaaa4000@naver.com>
2025-01-02 17:08:54 +0900

    feat: useHls 데이터 레벨 및 비트레이트 변화 핸들러 추가

--- src/hooks/useHls.ts
+++ src/hooks/useHls.ts
@@ -1,19 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Hls from 'hls.js';
+import type { ManifestParsedData, Level } from 'hls.js';
 
 export const useHls = (
   url: string,
   videoRef: React.RefObject<HTMLVideoElement>,
 ) => {
+  const [levels, setLevels] = useState<Level[]>([]);
+  const [currentBitrate, setCurrentBitrate] = useState(-1);
+  const hlsRef = useRef<Hls | null>(null);
+
   useEffect(() => {
     if (!videoRef.current || !Hls.isSupported()) return;
 
     const video = videoRef.current;
     const hls = new Hls();
 
-    const attemptPlay = async () => {
+    hlsRef.current = hls;
+    const attemptPlay = async (_: unknown, data: ManifestParsedData) => {
       try {
         await video.play();
+        setLevels(data.levels);
       } catch (error) {
         console.log('자동재생 실패:', error);
       }
@@ -24,8 +31,21 @@ export const useHls = (
 
     hls.on(Hls.Events.MANIFEST_PARSED, attemptPlay);
 
+    hls.on(Hls.Events.LEVEL_SWITCHED, (_, data) => {
+      setCurrentBitrate(data.level);
+    });
+
     return () => {
       hls.destroy();
     };
   }, []);
+
+  const changeBitrate = (bitrate: number) => {
+    if (!hlsRef.current) return;
+
+    hlsRef.current.currentLevel = bitrate; // -1: 자동, 0~n: 특정 품질
+    setCurrentBitrate(bitrate);
+  };
+
+  return { bitrate: currentBitrate, changeBitrate, levels };
 };

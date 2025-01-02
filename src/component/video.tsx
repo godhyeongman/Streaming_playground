f1fbecce939e997e007d2a200511e81006683ffe
dott <aaaa4000@naver.com>
2025-01-02 10:02:34 +0900

    refactor: 예제 코드 제거

--- src/component/video.tsx
+++ src/component/video.tsx
@@ -3,56 +3,17 @@ import { useRef, useState } from 'react';
 import { useHls } from '@/src/hooks/useHls';
 import PlayButton from '@/src/component/playButton';
 
-export default function Video() {
+
+type VideoProps = {
+  url: string;
+  width?: number;
+  height?: number;
+};
+export default function Video({ url, width, height }: VideoProps) {
   const videoRef = useRef<HTMLVideoElement>(null);
   const [playVideo, setPlayVideo] = useState(false);
   const [mouseHover, setMouseHover] = useState(false);
-  useHls('http://localhost:8080/hls/1080/123123_1080p.m3u8', videoRef);
-
-  // const [duration, setDuration] = useState(0); // 비디오 전체 길이
-
-  // const handleTimeUpdate = () => {
-  //   if (videoRef.current) {
-  //     const currentTime = videoRef.current.currentTime;
-  //     console.log(`Current Time: ${currentTime.toFixed(2)} seconds`);
-  //   }
-  // };
-
-  // const handleProgress = () => {
-  //   if (videoRef.current) {
-  //     const buffered = videoRef.current.buffered;
-  //     if (buffered.length > 0) {
-  //       const bufferedEnd = buffered.end(buffered.length - 1); // 버퍼링된 마지막 위치
-  //       console.log(`Buffered Time: ${bufferedEnd.toFixed(2)} seconds`);
-  //     }
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   if (videoRef.current) {
-  //     // 비디오가 로드될 때 전체 길이 설정
-  //     const handleLoadedMetadata = () => {
-  //       if (videoRef.current) {
-  //         const duration = videoRef.current.duration;
-  //         setDuration(duration);
-  //         console.log(`Video Duration: ${duration.toFixed(2)} seconds`);
-  //       }
-  //     };
-
-  //     // 이벤트 리스너 추가
-  //     videoRef.current.addEventListener('loadedmetadata', handleLoadedMetadata);
-  //     videoRef.current.addEventListener('progress', handleProgress); // 버퍼링 업데이트
-
-  //     // 클린업 함수로 이벤트 리스너 제거
-  //     return () => {
-  //       videoRef.current?.removeEventListener(
-  //         'loadedmetadata',
-  //         handleLoadedMetadata,
-  //       );
-  //       videoRef.current?.removeEventListener('progress', handleProgress);
-  //     };
-  //   }
-  // }, []);
+  useHls(url, videoRef);
 
   const handleClick = () => {
     const video = videoRef.current;
@@ -83,12 +44,10 @@ export default function Video() {
       <video
         ref={videoRef}
         autoPlay
-        // onTimeUpdate={handleTimeUpdate} // 현재 재생 시간 업데이트
         onClick={handleClick}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-        className="absolute w-full h-full"
-      ></video>
+      />
       {mouseHover && (
         <PlayButton
           size={100}
